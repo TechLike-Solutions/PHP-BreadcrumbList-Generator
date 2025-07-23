3e7827bc449e749cc01c7a59c36ecc98ec3bd464
Bhumit Gada <35927850+bhumitg@users.noreply.github.com>
2025-07-23 22:52:48 +0530

    Create breadcrumb-generator.php

---
+++ breadcrumb-generator.php
@@ -0,0 +1,37 @@
+<script type="application/ld+json">
+{
+	"@context": "https://schema.org/",
+	"@type": "BreadcrumbList",
+	"itemListElement": [<?php
+						$home = 'Home';
+						$path = array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+						$base = 'https://' . $_SERVER['HTTP_HOST'] . '/';
+
+						$breadcrumbs = Array($home.'::'.$base);
+
+						$array_keys = array_keys($path);
+						$last = end($array_keys);
+
+						foreach ($path AS $x => $crumb) {
+							$title = ucwords(str_replace(Array('.php', '-'), Array('', ' '), $crumb));
+
+							if($x != $last){
+								$breadcrumbs[] = $title.'::'.$base.$crumb.'/';
+								$base = $base.$crumb.'/';
+							}else{
+								$breadcrumbs[] = $title.'::'.$base.$crumb;
+								$base = $base.$crumb;
+							}
+						}
+
+						foreach ($breadcrumbs as $key => $val) {
+							list($name, $item) = explode("::", $val);
+							echo '{"@type": "ListItem",';
+							echo '"position": '.($key+1).',';
+							echo '"name": "'.$name.'",';
+							echo '"item": "'.$item.'"}';
+							if($key<count($breadcrumbs)-1){echo ',';}
+						}
+					?>]
+}
+</script>
